5133fdada077593111317cf6328ede33d9eb76ed
Federico1077 <92572879+Federico1077@users.noreply.github.com>
2021-12-15 17:57:43 +0100

    Create R_code_copernicus.r

---
+++ R_code_copernicus.r
@@ -0,0 +1,8 @@
+# R code for uploading and visualizing Copernicus data
+
+# install.packages("ncdf4")
+# setwd("C:/lab/copernicus")
+# library(raster)
+
+snow20211214 <- raster("ls_SCE_202112140000_NHEMI_VIIRS_V1.0.1.nc")
+snow20211214
