c19792b596f2c3d0a91d02fd4adc2e3bca8f5f65
Federico1077 <92572879+Federico1077@users.noreply.github.com>
2021-12-16 12:16:08 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -3,6 +3,42 @@
 # install.packages("ncdf4")
 # setwd("C:/lab/copernicus")
 # library(raster)
+# library(RStoolbox)
+# library(ggplot2)
+# library(viridis)
+
 
 snow20211214 <- raster("ls_SCE_202112140000_NHEMI_VIIRS_V1.0.1.nc")
 snow20211214
+
+# to see all the layers use brick function
+
+
+cl <- colorRampPalette(c('yellow','orange','red'))(100)
+plot(snow20211214, col=cl)
+
+cl <- colorRampPalette(c("dark blue","blue","light blue"))(100)
+plot(snow20211214, col=cl)
+
+#########
+
+# ggplot function
+ggplot() +
+geom.raster(snow20211214, mapping = aes(x=X, y=y. fill=Snow.Cover.Extent))
+
+
+# ggplot function with viridis
+ggplot() +
+geom_raster(snow20211214, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent)) +
+scale_fill_viridis()
+
+
+ggplot() +
+geom_raster(snow20211214, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent)) +
+scale_fill_viridis(option="cividis")
++
+ggtitle("cividis palette")
+
+
+
+
