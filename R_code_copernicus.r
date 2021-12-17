b15c0a8ea889774bcc9fc09d7fc7422159b2cb2a
Federico1077 <92572879+Federico1077@users.noreply.github.com>
2021-12-17 10:41:08 +0100

    Update R_code_copernicus.r

--- R_code_copernicus.r
+++ R_code_copernicus.r
@@ -39,6 +39,52 @@ scale_fill_viridis(option="cividis")
 +
 ggtitle("cividis palette")
 
+##############################################
 
+rlist <- list.files(pattern="SCE")
+rlist
 
+list_rast <- lapply(rlist, raster)
+list_rast
 
+snowstack <- stack(list_rast)
+snowstack
+
+ssummer <- snowstack$Snow.Cover.Extent.1
+swinter <- snowstack$Snow.Cover.Extent.2
+
+p1 <- ggplot() +
+geom_raster(ssummer, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.1)) +
+scale_fill_viridis(option="magma") +
+ggtitle ("snow cover in summer")
+
+p2 <- ggplot() +
+geom_raster(swinter, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.2)) +
+scale_fill_viridis(option="viridis") +
+ggtitle ("snow cover during freezing winter")
+
+# let's patchwork them togheter
+
+p1/p2
+
+# you can crop your image on a certain area
+
+# longitude from 0 to 20
+# latitude from 30 to 50
+
+# crop the stack to the extent of Sicily
+ext <- c(0, 20, 30, 50)
+ssummer_cropped <- crop(ssummer, ext)
+swinter_cropped <- crop(swinter, ext)
+
+p1 <- ggplot() +
+geom_raster(ssummer_cropped, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.1)) +
+scale_fill_viridis(option="magma") +
+ggtitle ("snow cover in summer")
+
+p2 <- ggplot() +
+geom_raster(swinter_cropped, mapping = aes(x=x, y=y, fill=Snow.Cover.Extent.2)) +
+scale_fill_viridis(option="viridis") +
+ggtitle ("snow cover during freezing winter")
+
+p1/p2
